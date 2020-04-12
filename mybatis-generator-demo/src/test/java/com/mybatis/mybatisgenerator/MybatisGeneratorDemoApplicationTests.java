20392202e7d7c94ef654eb6b3c9d41d96bc93383
puff <puff175088457@gmail.com>
2020-04-12 21:09:50 +0800

    first commit

---
+++ mybatis-generator-demo/src/test/java/com/mybatis/mybatisgenerator/MybatisGeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.mybatisgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
